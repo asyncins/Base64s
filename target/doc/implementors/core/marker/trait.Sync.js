393f2fca6b5f493fabcf3c85b33b6b32b7a46683
asyncins <asyncins@aliyun.com>
2019-08-21 21:29:27 +0800

    bases64 done

---
+++ target/doc/implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["bases64"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"bases64/bases/struct.Bases.html\" title=\"struct bases64::bases::Bases\">Bases</a>",synthetic:true,types:["bases64::bases::Bases"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
